e517f89dfe5865565575dcc9312384e12bf85283
Tarun22 <tarundeep15singh@gmail.com>
2022-03-02 15:49:07 +0530

    initial commit

---
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder , FormGroup} from '@angular/forms'
+@Component({
+  selector: 'app-employee-dashboard',
+  templateUrl: './employee-dashboard.component.html',
+  styleUrls: ['./employee-dashboard.component.css']
+})
+export class EmployeeDashboardComponent implements OnInit {
+ 
+  formvalue !: FormGroup;
+  constructor(private formbuilber: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.formvalue = this.formbuilber.group({
+      firstName : [''],
+      lastName : [''],
+      email : [''],
+      mobile : [''],
+      salary : ['']
+    })
+  }
+
+}
